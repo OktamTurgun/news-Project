9038831905b4e343a4d69a14d27040fb26c535d8
Uktam <uktamturgunov30@gmail.com>
2026-03-21 19:55:32 +0500

    fix: remove duplicate profile signal, use get_or_create

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
@@ -13,10 +10,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
-
-
-# User yaratilganda avtomatik Profile ham yaratiladi
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.get_or_create(user=instance)
--- accounts/signals.py
+++ accounts/signals.py
@@ -1,4 +1,3 @@
-# accounts/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
@@ -7,13 +6,10 @@ from .models import Profile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    """Yangi user yaratilganda avtomatik Profile yaratadi"""
     if created:
-        Profile.objects.create(user=instance)
-
+        Profile.objects.get_or_create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    # Profil mavjud bo'lsa saqlaydi, aks holda xato bermaydi
     if hasattr(instance, 'profile'):
         instance.profile.save()
